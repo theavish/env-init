3d0d3a3df26c6499ceaa0193ed4f2cf346011880
Avi Samloff <avi.samloff@gmail.com>
2016-03-08 16:47:36 -0800

    adds dropbox install

--- init.sh
+++ init.sh
@@ -131,6 +131,9 @@ brew cask install --appdir="/Applications" spotifree
 echo '*** install vlc ***'
 brew cask install --appdir="/Applications" vlc
 
+echo '*** install dropbox ***'
+brew cask install --appdir="/Applications" dropbox
+
 echo '*** install ohmyzsh ***'
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
